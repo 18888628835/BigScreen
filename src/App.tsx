0d41812934c1935c2311455572b5a518627e3b2e
邱彦兮 <qiuyanxi1992@gmail.com>
2021-09-09 17:01:27 +0800

    init

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import Index from './components';
+
+function App() {
+  return (
+    <div className='App'>
+      <Index />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Index = () => {
+  return <section>云南省大中型水利水电工程搬迁安置数字管理平台</section>;
+};
+
+export default Index;
